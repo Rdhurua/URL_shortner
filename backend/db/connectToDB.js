8cc21182cae750cbf02e44d0b11eeeecd905ff83
Rinku Dhurua <rinkudhurua258@gmail.com>
2025-08-12 21:00:29 +0530

    just-db-buffering time changed

--- backend/db/connectToDB.js
+++ backend/db/connectToDB.js
@@ -1,15 +1,17 @@
 import mongoose from "mongoose";
 
 const connectToDB=async ()=>{
-     try{
-         await mongoose.connect(process.env.MONGO_URI);
-         console.log("connected to database");
-
-         
-     }
-     catch(error){
-         console.log("error during mongo-connection",error);
-     }
+     try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, 
+    });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(" MongoDB connection error:", err.message);
+    process.exit(1);
+  }
 }
 
 export default connectToDB;
